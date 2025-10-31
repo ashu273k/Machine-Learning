e3ba5712824d5685bc8c762e36dcf77b8cb5c285
Aashu Kumar <ashu9472.as@gmail.com>
2025-10-31 15:55:38 +0530

    String

---
+++ Python/String&Text/bulletPointAdder.py
@@ -0,0 +1,12 @@
+import pyperclip
+text = pyperclip.paste()
+
+# TODO: Seperate lines and add stars.
+
+lines = text.split('\n')
+for i in range(len(lines)):  # Loop through all indexes in the "lines" list.
+    lines[i] = '* ' + lines[i] # Add a star to each string in the "lines" list.
+
+text = '\n'.join(lines)
+print(text)
+pyperclip.copy(text)
